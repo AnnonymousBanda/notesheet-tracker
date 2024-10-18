a8f56bba6fd98f13ab82a2b37634cd702230a797
Ankit Bhagat <annonymous007banda@gmail.com>
2024-10-18 21:44:42 +0530

    feat(security): enhance API security with rate limiting, input sanitization, and CSP

--- server/app.js
+++ server/app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const passport = require('passport')
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
+const mongoSanitize = require('express-mongo-sanitize')
+// const xss = require('xss-clean')
 
 const configPassport = require('./config/oauth.config')
 const { apiRouter, authRouter, oauthRouter } = require('./routes')
@@ -19,24 +23,41 @@ const corsOptions = {
 	credentials: true,
 	optionsSuccessStatus: 200,
 }
+// const helmetConfig = helmet.contentSecurityPolicy({
+// 	directives: {
+// 		defaultSrc: ["'self'"],
+// 		scriptSrc: ["'self'", 'https://trustedscripts.example.com'],
+// 		objectSrc: ["'none'"],
+// 		upgradeInsecureRequests: [],
+// 	},
+// })
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+	message: 'Too many requests from this IP, please try again later.',
+})
+
 app.use(cors(corsOptions))
+app.use(helmet())
+// app.use(helmetConfig)
+app.use(mongoSanitize())
+app.use(limiter)
 
 app.use(morgan('dev'))
+
+app.use(passport.initialize())
+configPassport(passport)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(express.static('public'))
 
-app.use(passport.initialize())
-configPassport(passport)
-
 app.use('/auth', authRouter)
 app.use('/oauth', oauthRouter)
 app.use('/api', apiRouter)
 
 app.route('*').all(notFound)
-
 app.use(globalErrorController)
 
 module.exports = app
