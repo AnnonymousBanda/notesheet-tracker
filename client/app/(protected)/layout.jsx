308b3cb09b4a5992fb83e0e435457e9a9ae51eab
VinayKhedkar <arithmetic.chip@gmail.com>
2024-10-21 19:07:52 +0530

    remove: remove sidebar

--- client/app/(protected)/layout.jsx
+++ client/app/(protected)/layout.jsx
@@ -1,25 +1,14 @@
 'use client'
-
-import Sidebar from '@/components/sidebar'
 import Navbar from '@/components/navbar'
-import { useState } from 'react'
 import Protected from '@/hoc/Protected'
 
 const layout = ({ children }) => {
-	const [issidebarOpen, setisSidebarOpen] = useState(false)
 
 	return (
 		<Protected>
 			<main className='flex h-screen w-screen max-container p-4 gap-4'>
-				<Sidebar
-					isSidebarOpen={issidebarOpen}
-					setisSidebarOpen={setisSidebarOpen}
-				/>
 				<div className='w-full flex flex-col gap-5'>
-					<Navbar
-						isSidebarOpen={issidebarOpen}
-						setisSidebarOpen={setisSidebarOpen}
-					/>
+					<Navbar/>
 					<div className='bg-gray-200 h-full rounded-lg p-5'>
 						{children}
 					</div>
--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,95 +1,39 @@
-'use client'
-import gsap from 'gsap'
-import React from 'react'
-import { useEffect } from 'react'
-import { useAuth } from '@/contexts/AuthContext'
-import { DynamicLazyBlurImage, LazyBlurImage } from './LazyBlurImage'
+"use client";
+import React, { useState } from "react";
+import { useAuth } from "@/contexts/AuthContext";
+import { DynamicLazyBlurImage, LazyBlurImage } from "./LazyBlurImage";
+import Link from "next/link";
 
-export default function Navbar({ isSidebarOpen, setisSidebarOpen }) {
-	const { user } = useAuth()
-	const tl = gsap.timeline()
-	useEffect(() => {
-		if (isSidebarOpen) {
-			tl.to('.one', {
-				rotate: '45deg',
-				y: '0.5rem',
-				duration: 0.5,
-			})
-			tl.to(
-				'.two',
-				{
-					opacity: 0,
-					duration: 0.5,
-				},
-				'-=0.5'
-			)
-			tl.to(
-				'.three',
-				{
-					rotate: '-45deg',
-					y: '-0.5rem',
-					duration: 0.5,
-				},
-				'-=0.5'
-			)
-		} else {
-			tl.to('.one', {
-				rotate: '0deg',
-				y: '0rem',
-				duration: 0.5,
-			})
-			tl.to(
-				'.three',
-				{
-					rotate: '0deg',
-					y: '0rem',
-					duration: 0.5,
-				},
-				'-=0.5'
-			)
-			tl.to(
-				'.two',
-				{
-					opacity: 1,
-					delay: 0.3,
-					duration: 0.3,
-				},
-				'-=0.5'
-			)
-		}
-	}, [isSidebarOpen])
-	return (
-		<div className='w-full bg-gray-200 h-[8vh] px-8 py-4 flex justify-between items-center rounded-lg'>
-			<div
-				onClick={() => {
-					setisSidebarOpen(!isSidebarOpen)
-				}}
-				className='relative z-[100] flex flex-col gap-1 lg:hidden'
-			>
-				<div className='one relative w-8 h-1 bg-black'></div>
-				<div className='two relative w-8 h-1 bg-black'></div>
-				<div className='three relative w-8 h-1 bg-black'></div>
-			</div>
-			<h4>DASHBOARD</h4>
-			<div className='cursor-pointer h-full flex justify-center items-center'>
-				{!!user?.photoURL ? (
-					<DynamicLazyBlurImage
-						src={user.photoURL}
-						alt='profile picture'
-						width={35}
-						height={35}
-						className='p-[3rem]'
-					/>
-				) : (
-					<LazyBlurImage
-						src='user.png'
-						alt='profile picture'
-						width={40}
-						height={40}
-						className='p-[0.1rem]'
-					/>
-				)}
-			</div>
-		</div>
-	)
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const { user } = useAuth();
+
+  return (
+    <div className="w-full bg-gray-200 h-[8vh] px-8 py-4 flex justify-between items-center rounded-lg">
+      <h4>DASHBOARD</h4>
+      <div
+        onClick={toggleMenu}
+        className="cursor-pointer h-full flex justify-center items-center"
+      >
+        {user?.photoURL ? (
+          <DynamicLazyBlurImage
+            src={user.photoURL}
+            alt="profile picture"
+            width={35}
+            height={35}
+            className="p-[3rem]"
+          />
+        ) : (
+          <LazyBlurImage
+            src="user.png"
+            alt="profile picture"
+            width={40}
+            height={40}
+            className="p-[0.2rem]"
+          />
+        )}
+      </div>
+    </div>
+  );
 }
