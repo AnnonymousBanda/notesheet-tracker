9c3f9e5b84d9c1d9614cd6d4dacebe7ce4372a86
VinayKhedkar <arithmetic.chip@gmail.com>
2024-10-23 18:09:50 +0530

    fix: fix new notesheet form overflow bug

--- client/app/(protected)/new-notesheet/page.jsx
+++ client/app/(protected)/new-notesheet/page.jsx
@@ -15,15 +15,16 @@ const NewNotesheetForm = () => {
   const router = useRouter();
 
   const onSubmit = (data) => {
+    data.approvals.push("adean");
     console.log("Form Data:", data);
     reset();
     setPdfFile(null);
-	setPdfFileUrl(null);
-	openDialog("Notesheet submitted successfully");
-	setTimeout(() => {
-		router.push('/');
-		onClose();
-	}, 1500);
+    setPdfFileUrl(null);
+    openDialog("Notesheet submitted successfully");
+    setTimeout(() => {
+      router.push("/");
+      onClose();
+    }, 1500);
   };
 
   const onError = (errorList) => {
@@ -67,24 +68,30 @@ const NewNotesheetForm = () => {
   return (
     <form
       onSubmit={handleSubmit(onSubmit, onError)}
-      className="flex items-center justify-start gap-10 w-full p-4 overflow-auto"
+      className="flex items-center justify-start gap-10 w-full p-4 h-full"
     >
       <div
         onDrop={handleDrop}
         onDragOver={(e) => e.preventDefault()}
-        className="overflow-hidden h-full flex flex-col items-center justify-center text-center w-1/3 hover:bg-gray-100 border-gray-400 hover:border-black border-dashed border-[4px] rounded-lg gap-8 transition-all duration-500"
+        className="overflow-hidden h-full flex flex-col items-center justify-center text-center w-1/3 hover:bg-gray-100 border-gray-400 hover:border-black border-dashed border-[4px] rounded-lg gap-8 transition-all duration-500 flex-wrap px-4"
       >
-        <h4 className="xl:w-[40rem] w-[25rem] overflow-hidden text-ellipsis">
+        <h4 className="px-4 lg:text-[2rem] text-[1.5rem] break-words whitespace-normal text-center overflow-hidden text-ellipsis">
           {pdfFile ? (
             <a className="text-blue-400 text-wrap" target="#" href={pdfFileUrl}>
-				{pdfFile.name}
+              {pdfFile.name}
             </a>
           ) : (
             "Please drag and drop or upload a pdf"
           )}
         </h4>
         {pdfFile ? (
-          <button onClick={()=> {setPdfFile(null); setPdfFileUrl(null)}} className="w-[12rem] flex items-center justify-center bg-[#2f2f2f] text-white h-[40px] px-4 rounded-sm hover:bg-[#0e0202] text-[1.5rem]">
+          <button
+            onClick={() => {
+              setPdfFile(null);
+              setPdfFileUrl(null);
+            }}
+            className="w-[12rem] flex items-center justify-center bg-[#2f2f2f] text-white h-[40px] px-4 rounded-sm hover:bg-[#0e0202] text-[1.5rem]"
+          >
             <h4>Reset PDF</h4>
           </button>
         ) : (
@@ -92,16 +99,15 @@ const NewNotesheetForm = () => {
             type="file"
             accept="application/pdf"
             onChange={handleFileUpload}
-            className="xl:text-[2rem] text-[1.5rem]"
-			required
+            className="xl:text-[2rem] lg:text-[1.5rem] text-[1.2rem]"
+            required
           />
         )}
       </div>
-      <div className="h-5/6 w-1 bg-black"></div>
-      <div className="flex flex-col justify-start h-full w-1/2 gap-10 ">
+      <div className="flex flex-col justify-start h-full w-2/3 gap-10 overflow-auto">
         <h3 className="text-gray-700 font-semibold">Notesheet Details</h3>
         <div className="w-full flex flex-col gap-4">
-          <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-3 w-2/3">
             <label className="text-[2rem] font-medium text-gray-700">
               Subject
             </label>
@@ -115,13 +121,13 @@ const NewNotesheetForm = () => {
             />
           </div>
 
-          <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-3 w-fit">
             <label className="block text-[2rem] font-medium text-gray-700">
               Date
             </label>
             <input
               {...register("date")}
-              className="text-[2rem] border-gray-400 focus:border-blue-400 border-solid w-full p-2 cursor-not-allowed"
+              className="text-[2rem] border-gray-400 focus:border-blue-400 border-solid p-2 cursor-not-allowed"
               type="date"
               value={date}
               placeholder="dd/mm/yyyy"
@@ -129,7 +135,7 @@ const NewNotesheetForm = () => {
             />
           </div>
 
-          <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-3 w-fit">
             <label className="text-[2rem] font-medium text-gray-700">
               Raised By
             </label>
@@ -145,7 +151,7 @@ const NewNotesheetForm = () => {
             />
           </div>
 
-          <div className="flex flex-col gap-3">
+          <div className="flex flex-col gap-3 w-1/2">
             <label className="text-[2rem] font-medium text-gray-700">
               Amount
             </label>
@@ -160,14 +166,17 @@ const NewNotesheetForm = () => {
             />
           </div>
 
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 w-2/3">
             <label className="text-[2rem] font-medium text-gray-700">
               Approvals needed
             </label>
             <div className="flex flex-wrap gap-10">
               {["GenSec", "PIC", "VP Gymkhana", "ARSA", "DrSA"].map(
                 (approval) => (
-                  <label key={approval} className="flex items-center cursor-pointer">
+                  <label
+                    key={approval}
+                    className="flex items-center cursor-pointer"
+                  >
                     <input
                       type="checkbox"
                       {...register("approvals", {
@@ -184,11 +193,10 @@ const NewNotesheetForm = () => {
               <label key="adean" className="flex items-center">
                 <input
                   type="checkbox"
-                  {...register("approvals", {
-                    required: "Please select at least one approval",
-                  })}
                   value="adean"
-                  className="mr-2 w-7 h-7 border-black border-solid bg-transparent text-[2rem]"
+                  checked={true}
+                  disabled
+                  className="mr-2 w-7 h-7 border-black border-solid bg-transparent text-[2rem] cursor-not-allowed"
                 />
                 <p className="text-[2rem]">Adean</p>
               </label>
@@ -197,7 +205,7 @@ const NewNotesheetForm = () => {
         </div>
         <button
           type="submit"
-          className="w-[15rem] flex items-center justify-center bg-[#2f2f2f] text-white h-[45px] px-4 rounded-sm hover:bg-[#0e0202] text-[1.7rem]"
+          className="mb-3 w-[15rem] flex items-center justify-center bg-[#2f2f2f] text-white min-h-[45px] px-4 rounded-sm hover:bg-[#0e0202] text-[1.7rem]"
         >
           <h4>Submit</h4>
         </button>
