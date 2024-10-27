a8fdecf1745e696ada5a44ea182d7ca0335e2398
VinayKhedkar <arithmetic.chip@gmail.com>
2024-10-27 22:05:27 +0530

    refactor: refactor add button on dashboard

--- client/app/(protected)/(home)/page.jsx
+++ client/app/(protected)/(home)/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import AdminDashboard from "@/components/AdminDashboard";
+import UserDashboard from "@/components/UserDashboard";
 import { useAuth } from "@/contexts/AuthContext";
 import Link from "next/link";
 import { useState } from "react";
@@ -7,32 +9,7 @@ import { useState } from "react";
 const Dashboard = () => {
   const { isAdmin } = useAuth();
   const [admin] = useState(isAdmin());
-
-  const UserDashboard = () => {
-    return (
-      <div>
-        <Link
-          href="/new-notesheet"
-          className="fixed bottom-12 right-12 flex justify-center items-center bg-[#04AA6D] hover:bg-[#3bb588] p-4 rounded-xl">
-          <img src="/images/plus.svg" alt="" className="w-12" />
-        </Link>
-      </div>
-    );
-  };
-
-  const AdminDashboard = () => {
-    return (
-      <>
-        <div className="flex flex-col gap-3"></div>
-        <Link
-          href="/new-notesheet"
-          className="fixed bottom-12 right-12 flex justify-center items-center bg-[#04AA6D] hover:bg-[#3bb588] p-4 rounded-xl">
-          <img src="/images/plus.svg" alt="" className="w-12" />
-        </Link>
-      </>
-    );
-  };
-
+ 
   return admin ? <AdminDashboard /> : <UserDashboard />;
 };
 
