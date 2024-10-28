95a826b9258a2b4f9e164d97e9cd21976cd4990b
VinayKhedkar <arithmetic.chip@gmail.com>
2024-10-28 12:44:06 +0530

    fix: fix new notesheet button

--- client/app/(protected)/(home)/page.jsx
+++ client/app/(protected)/(home)/page.jsx
@@ -1,16 +1,42 @@
 "use client";
 
 import AdminDashboard from "@/components/AdminDashboard";
+import Pagination from "@/components/Pagination";
 import UserDashboard from "@/components/UserDashboard";
 import { useAuth } from "@/contexts/AuthContext";
 import Link from "next/link";
-import { useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Dashboard = () => {
   const { isAdmin } = useAuth();
   const [admin] = useState(isAdmin());
- 
-  return admin ? <AdminDashboard /> : <UserDashboard />;
+
+  const pathname = usePathname();
+  const searchparams = useSearchParams();
+  const params = new URLSearchParams(searchparams);
+  const { replace } = useRouter();
+
+  useEffect(() => {
+    if (!params.toString()) {
+      params.set("page", "1");
+      if (admin) params.set("type", "raised");
+      replace(`${pathname}?${params.toString()}`);
+    }
+  }, []);
+
+  return (
+    <>
+      {admin ? <AdminDashboard /> : <UserDashboard />}
+      <Link
+        href="/new-notesheet"
+        className="fixed z-10 bottom-8 right-8 flex justify-center items-center bg-black p-4 hover:bg-[#3a3a3a] rounded-xl"
+      >
+        <img src="/images/plus.svg" alt="" className="w-12 " />
+      </Link>
+      <Pagination />
+    </>
+  );
 };
 
 export default Dashboard;
